991d7ce34a904a5cd621fd5631cd969178c3d80c
in5net <48165199+in5net@users.noreply.github.com>
2022-09-14 21:31:15 -0400

    -seek accepts a time in HH:MM:SS format fix

--- src/commands/seek.ts
+++ src/commands/seek.ts
@@ -32,7 +32,7 @@ export default command(
 );
 
 function str2Seconds(str: string): number {
-  const parts = str.split(':').map(parseInt);
+  const parts = str.split(':').map(x => parseInt(x));
   switch (parts.length) {
     case 1: {
       const [seconds] = parts;
@@ -42,26 +42,19 @@ function str2Seconds(str: string): number {
     }
     case 2: {
       const [minutes, seconds] = parts;
-      if (
-        minutes === undefined ||
-        isNaN(minutes) ||
-        seconds === undefined ||
-        isNaN(seconds)
-      )
-        throw new Error('Invalid time');
+      if (minutes === undefined || isNaN(minutes))
+        throw new Error('Invalid minutes');
+      if (seconds === undefined || isNaN(seconds))
+        throw new Error('Invalid seconds');
       return minutes * 60 + seconds;
     }
     case 3: {
       const [hours, minutes, seconds] = parts;
-      if (
-        hours === undefined ||
-        isNaN(hours) ||
-        minutes === undefined ||
-        isNaN(minutes) ||
-        seconds === undefined ||
-        isNaN(seconds)
-      )
-        throw new Error('Invalid time');
+      if (hours === undefined || isNaN(hours)) throw new Error('Invalid hours');
+      if (minutes === undefined || isNaN(minutes))
+        throw new Error('Invalid minutes');
+      if (seconds === undefined || isNaN(seconds))
+        throw new Error('Invalid seconds');
       return hours * 3600 + minutes * 60 + seconds;
     }
     default:
